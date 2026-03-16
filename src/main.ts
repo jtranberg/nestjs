5796a6debd7c006aa8deda140eefd081985ebbf6
jtranberg <jtranberg@hotmail.ca>
2026-03-15 23:08:15 -0700

    dist build

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(process.env.PORT || 3000);
-  console.log(`Server running );
+  console.log(`Server running`);
 }
 bootstrap();
